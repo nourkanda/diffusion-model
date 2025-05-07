9543719b158f65791b75404e865000b9b8eb5067
Nour Kandalaft <nkandala@asu.edu>
2025-05-07 23:58:46 +0000

    Start adding unit tests to diffusion model.

---
+++ test_diffusion.py
@@ -0,0 +1,10 @@
+"""Tests for the diffusion model module."""
+
+from diffusion import calculate_stable_time_step
+
+
+def test_time_step_is_float():
+    time_step = calculate_stable_time_step(1,1)
+    assert isinstance(time_step, float)
+    
+    
