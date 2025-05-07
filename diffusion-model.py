a997310fe025ccbaaa212784d3b2f3954fe790fb
Nour Kandalaft <nkandala@asu.edu>
2025-05-07 23:31:14 +0000

    Add plot_profile function

--- diffusion-model.py
+++ diffusion-model.py
@@ -4,6 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
+def plot_profile(x, cake,color="r"):  
+    """Display the cake profile."""
+    plt.figure()
+    plt.plot(x, cake, color)
+    plt.xlabel("x")
+    plt.ylabel("cake")
+    plt.title("Cake Profile")
+
 D = 100
 Lx = 300
 
@@ -11,29 +19,13 @@ dx = 0.5
 x = np.arange(start=0, stop=Lx, step=dx)
 nx = len(x)
 
-nx
-
-x[-1]
-
-x[0]
-
-x[-2]
-
-x[0:5]
-
-x[-5:]
-
 C=np.zeros_like(x) 
 C_left=500
 C_right=0
 C[x <= Lx/2] = C_left
 C[x > Lx / 2] = C_right
 
-plt.figure()
-plt.plot(x, C, "r")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Initial Profile")
+plot_profile(x, C)
 
 nt = 5000
 dt = 0.5 * dx ** 2 / D
@@ -41,8 +33,6 @@ dt = 0.5 * dx ** 2 / D
 for t in range(0, nt):
 	C[1:-1] += D * dt / dx ** 2 * (C[:-2] - 2*C[1:-1] + C[2:])
 
-plt.plot(x, C, "b")
-plt.xlabel("x")
-plt.ylabel("C")
-plt.title("Final Profile")
+plot_profile (x, C, color="b")
+
 
